c4779a050c7c8991f43f51a59d4c55f30d2989fe
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-13 01:21:00 +0400

    home work

---
+++ HomeWork1Csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork1Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            if (num % 2 == 0) 
+          {
+            Console.WriteLine("verilmish eded cutdur");
+          }
+            else
+            Console.WriteLine("verilmish eded tekdir");
+
+        }
+    }
+    
+}
---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1 =2;
+            int N2 = 2;
+
+        
+            if (N1 > N2) 
+            {
+                Console.WriteLine(N1);
+            }
+            else if (N1 < N2) 
+            {
+                Console.WriteLine(N2);
+            }
+            else
+            {
+                Console.WriteLine("sayi beraberdir");
+            }
+            Console.ReadKey();
+        }
+    } 
+}
